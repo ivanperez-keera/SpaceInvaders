fa88c66866649d683e8f47d681e6e5eee1dc5e89
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-01 05:38:57 +0000

    Alphabetize imports (KSG Haskell 1.3.0 - 3.5). Refs #40.
    Imports belonging to the same group should be listed in alphabetical
    order.

--- src/Main.hs
+++ src/Main.hs
@@ -8,18 +8,18 @@
 module Main where
 
 -- External imports
-import System.Random
-import Data.Point2 (Point2(..), point2Y)
-import Data.Maybe (isJust)
 import Data.Array
+import Data.Maybe (isJust)
+import Data.Point2 (Point2(..), point2Y)
 import FRP.Yampa
 import qualified Graphics.HGL as HGL
+import System.Random
 
 -- Internal imports
 -- Temporary, just to make sure all modules compile.
 import Animate
-import IdentityList
 import Colors
+import IdentityList
 import Object
 import ObjectBehavior
 import Parser
