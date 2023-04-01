a3d99244153b977813aa9f1ab4065a063190414f
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-01 05:12:55 +0000

    Format module header using haddock (KSG Haskell 1.3.0 - 3.3). Refs #40.
    All Haskell files must include a copyright notice in a comment. The
    copyright notice must be included in a format parseable by Haddock.

--- src/Diagnostics.hs
+++ src/Diagnostics.hs
@@ -1,16 +1,9 @@
-{-
-******************************************************************************
-*                              I N V A D E R S                               *
-*                                                                            *
-*       Module:         Diagnostics                                          *
-*       Purpose:        Standardized error-reporting for Invaders            *
-*       Authors:        Henrik Nilsson                                       *
-*                                                                            *
-*             Copyright (c) Yale University, 2003                            *
-*                                                                            *
-******************************************************************************
--}
-
+-- |
+-- Module      : Diagnostics
+-- Description : Standardized error-reporting for Invaders.
+-- Copyright   : (c) Yale University, 2003
+--
+-- Author: Henrik Nilsson
 module Diagnostics where
 
 usrErr mn fn msg = error (mn ++ "." ++ fn ++ ": " ++ msg)
