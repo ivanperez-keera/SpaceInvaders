cf2032429f94d63c9f34b69d34c3dea574a7932c
Ivan Perez <ivan.perez@keera.co.uk>
2023-04-01 06:04:30 +0000

    Add signatures (KSG Haskell 1.3.0 - 3.6). Refs #40.
    All top-level functions must include a type signature.

--- src/Diagnostics.hs
+++ src/Diagnostics.hs
@@ -6,7 +6,9 @@
 -- Author: Henrik Nilsson
 module Diagnostics where
 
+usrErr :: String -> String -> String -> a
 usrErr mn fn msg = error (mn ++ "." ++ fn ++ ": " ++ msg)
 
+intErr :: String -> String -> String -> a
 intErr mn fn msg = error ("[internal error] " ++ mn ++ "." ++ fn ++ ": "
                           ++ msg)
